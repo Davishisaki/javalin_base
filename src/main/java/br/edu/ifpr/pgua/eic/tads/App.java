1983b2e58d48e2c46b966da8cefcb12f9c316654
Davishisaki <davishisaki@gmail.com>
2024-12-06 12:43:34 -0300

    Acompanhando aula

--- src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/App.java
@@ -1,20 +1,25 @@
 package br.edu.ifpr.pgua.eic.tads;
 
+import br.edu.ifpr.pgua.eic.tads.controllers.CadastroController;
 import br.edu.ifpr.pgua.eic.tads.controllers.IndexController;
 import br.edu.ifpr.pgua.eic.tads.utils.JavalinUtils;
-import io.javalin.Javalin;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ){
+    public static void main(String[] args) {
         var app = JavalinUtils.makeApp(7070);
-        
+
         IndexController indexController = new IndexController();
-        
+        CadastroController cadastroController = new CadastroController();
+
         app.get("/", indexController.get);
-        
+        app.get("/ola", indexController.ola);
+
+        app.get("/cadastro", cadastroController.get);
+        app.post("/cadastro", cadastroController.post);
+
     }
 }
